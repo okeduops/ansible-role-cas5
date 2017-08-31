1635fc5f13ca1345a8918e797475c8eddb64224f
Jason Sherman <jsherman@usao.edu>
2017-08-31 10:25:59 -0500

    Add helper script for cleaning up idp metadata.

---
+++ files/cas_idp_metadata_cleanup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# a very quick and dirty cleanup of the IdP metadata as described here:
+# https://apereo.github.io/cas/5.0.x/installation/Configuring-SAML2-Authentication.html#idp-metadata
+
+# takes full path of idp-metadata.xml as an argument.
+# Prints massaged metadata to stdout.
+# Do with it what you will.
+
+cat $1 | sed -n 'H
+$ {g
+   s/<\!--/²/g;s/-->/³/g
+:a
+   s/²[^³]*³//g
+   t a
+   s/.//p
+   }'| sed -n 'H
+$ {g
+   s/<AttributeAuthorityDescriptor/²/g;s/<\/AttributeAuthorityDescriptor>/³/g
+:a
+   s/²[^³]*³//g
+   t a
+   s/.//p
+   }'| grep -Ev 'bindings:HTTP-POST-SimpleSign|bindings:SOAP|SingleLogoutService Binding="ur
+n:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect|urn:mace:shibboleth:1.0:profiles:AuthnReque
+st|^$|^\s*$'
